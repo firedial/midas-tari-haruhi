e0eb3361d18f26bc633fc3005052516a8cc14cce
firedial <42674772+firedial@users.noreply.github.com>
2021-10-24 19:29:30 +0900

    balance 追加用のページがいらなくなったのでとりあえず導線を削除

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -7,9 +7,6 @@
                     <router-link v-bind:to="{name: 'balance.list'}">
                         <button class="btn btn-success">Balance List</button>
                     </router-link>
-                    <router-link v-bind:to="{name: 'balance.create'}">
-                        <button class="btn btn-success">Balance Add</button>
-                    </router-link>
                     <router-link v-bind:to="{name: 'move.list', params: {attributeName: 'places'}}">
                         <button class="btn btn-success">Place Move List</button>
                     </router-link>
