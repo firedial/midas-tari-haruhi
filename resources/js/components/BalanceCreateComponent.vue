7dc953e5bbb66b6f69fe2772fd5d30fa3f467767
firedial <42674772+firedial@users.noreply.github.com>
2021-11-23 16:15:16 +0900

    入力画面をいい感じにした

--- resources/js/components/BalanceCreateComponent.vue
+++ resources/js/components/BalanceCreateComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-sm-6">
+            <div class="col-sm-7">
                 <form v-on:submit.prevent="submit">
                     <div class="form-group row">
                         <label for="amount" class="col-sm-3 col-form-label">Amount</label>
@@ -13,19 +13,34 @@
                     </div>
                     <div class="form-group row">
                         <label for="kind_element" class="col-sm-3 col-form-label">Kind Element</label>
-                        <input type="text" class="col-sm-9 form-control" id="kind_element" v-model="balance.kind_element_id">
+                        <select class="form-select form-select-sm" v-model="balance.kind_element_id">
+                            <option value=""></option>
+                            <option v-for="kindElement in kindElements" :value="kindElement.id" :key="kindElement.id">
+                                {{ kindElement.description }}
+                            </option>
+                        </select>
                     </div>
                     <div class="form-group row">
                         <label for="purpose_element" class="col-sm-3 col-form-label">Purpose Element</label>
-                        <input type="text" class="col-sm-9 form-control" id="purpose_element" v-model="balance.purpose_element_id">
+                        <select class="form-select form-select-sm" v-model="balance.purpose_element_id">
+                            <option value=""></option>
+                            <option v-for="purposeElement in purposeElements" :value="purposeElement.id" :key="purposeElement.id">
+                                {{ purposeElement.description }}
+                            </option>
+                        </select>
                     </div>
                     <div class="form-group row">
                         <label for="place_element" class="col-sm-3 col-form-label">Place Element</label>
-                        <input type="text" class="col-sm-9 form-control" id="purpose_element" v-model="balance.place_element_id">
+                        <select class="form-select form-select-sm" v-model="balance.place_element_id">
+                            <option value=""></option>
+                            <option v-for="placeElement in placeElements" :value="placeElement.id" :key="placeElement.id">
+                                {{ placeElement.description }}
+                            </option>
+                        </select>
                     </div>
                     <div class="form-group row">
                         <label for="date" class="col-sm-3 col-form-label">Date</label>
-                        <input type="text" class="col-sm-9 form-control" id="date" v-model="balance.date">
+                        <input type="date" class="col-sm-9 form-control" id="date" v-model="balance.date">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -38,16 +53,42 @@
     export default {
         data: function () {
             return {
-                balance: {}
+                balance: {},
+                kindElements: [],
+                purposeElements: [],
+                placeElements: []
             }
         },
         methods: {
+            getKindElements() {
+                axios.get('/api/attribute_elements/kind_element')
+                    .then((res) => {
+                        this.kindElements = res.data;
+                    });
+            },
+            getPurposeElements() {
+                axios.get('/api/attribute_elements/purpose_element')
+                    .then((res) => {
+                        this.purposeElements = res.data;
+                    });
+            },
+            getPlaceElements() {
+                axios.get('/api/attribute_elements/place_element')
+                    .then((res) => {
+                        this.placeElements = res.data;
+                    });
+            },
             submit() {
                 axios.post('/api/balances', this.balance)
                     .then((res) => {
                         this.$router.push({name: 'balance.list'});
                     });
             }
+        },
+        mounted() {
+            this.getKindElements();
+            this.getPurposeElements();
+            this.getPlaceElements();
         }
     }
 </script>
