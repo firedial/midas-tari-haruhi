a76bd58986aca73a99d200f8023c29583b86676f
firedial <42674772+firedial@users.noreply.github.com>
2021-11-23 15:33:34 +0900

    ヘッダーの修正

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid bg-dark mb-3">
         <div class="container">
             <nav class="navbar navbar-dark">
-                <span class="navbar-brand mb-0 h1">Vue Laravel SPA</span>
+                <span class="navbar-brand mb-0 h1">Midas Tari</span>
                 <div>
                     <router-link v-bind:to="{name: 'balance.list'}">
                         <button class="btn btn-success">Balance List</button>
@@ -12,6 +12,9 @@
                     </router-link>
                     <router-link v-bind:to="{name: 'move.create', params: {attributeName: 'places'}}">
                         <button class="btn btn-success">Place Move Add</button>
+                    </router-link>
+                    <router-link v-bind:to="{name: 'analyze'}">
+                        <button class="btn btn-success">Analyze</button>
                     </router-link>
                 </div>
             </nav>
