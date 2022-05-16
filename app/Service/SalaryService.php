4274936d99005af5c0e1f3975d4f4f1e64cfe794
firedial <42674772+firedial@users.noreply.github.com>
2022-05-16 22:01:47 +0900

    トランザクションの追加

--- app/Service/SalaryService.php
+++ app/Service/SalaryService.php
@@ -28,148 +28,154 @@ class SalaryService
 
     public static function registerSalary(array $salary): Bool
     {
-        $baseSalary = [
-            'amount' => (int)$salary['baseSalary'],
-            'item' => '基本給',
-            'kind_element_id' => self::SALARY_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($baseSalary);
+        \DB::beginTransaction();
+        try {
+            $baseSalary = [
+                'amount' => (int)$salary['baseSalary'],
+                'item' => '基本給',
+                'kind_element_id' => self::SALARY_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($baseSalary);
 
-        $adjustmentSalary = [
-            'amount' => (int)$salary['adjustmentSalary'],
-            'item' => '職務調整給',
-            'kind_element_id' => self::SALARY_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($adjustmentSalary);
+            $adjustmentSalary = [
+                'amount' => (int)$salary['adjustmentSalary'],
+                'item' => '職務調整給',
+                'kind_element_id' => self::SALARY_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($adjustmentSalary);
 
-        $transportation = [
-            'amount' => (int)$salary['transportation'],
-            'item' => '非課税通勤費',
-            'kind_element_id' => self::TRANSPORTATION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($transportation);
+            $transportation = [
+                'amount' => (int)$salary['transportation'],
+                'item' => '非課税通勤費',
+                'kind_element_id' => self::TRANSPORTATION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($transportation);
 
-        $holdingIncentives = [
-            'amount' => (int)$salary['holdingIncentives'],
-            'item' => '持株奨励金',
-            'kind_element_id' => self::HOLDING_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($holdingIncentives);
+            $holdingIncentives = [
+                'amount' => (int)$salary['holdingIncentives'],
+                'item' => '持株奨励金',
+                'kind_element_id' => self::HOLDING_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($holdingIncentives);
 
-        $transportationMove = [
-            'amount' => (int)$salary['transportation'],
-            'item' => '予算移動',
-            'before_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'after_id' => self::TRANSPORTATION_PURPOSE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        MoveDao::insertMoveByArray('purpose', $transportationMove);
+            $transportationMove = [
+                'amount' => (int)$salary['transportation'],
+                'item' => '予算移動',
+                'before_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'after_id' => self::TRANSPORTATION_PURPOSE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            MoveDao::insertMoveByArray('purpose', $transportationMove);
 
-        $deductionValue =
-            (int)$salary['healthInsurance'] +
-            (int)$salary['welfarePension'] +
-            (int)$salary['residentTax'] +
-            (int)$salary['employmentInsurance'] +
-            (int)$salary['incomeTax'] +
-            (int)$salary['holding'];
-        
-        $deductionMove = [
-            'amount' => $deductionValue,
-            'item' => '予算移動',
-            'before_id' => self::INCOME_PURPOSE_ELEMENT_ID,
-            'after_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        MoveDao::insertMoveByArray('purpose', $deductionMove);
+            $deductionValue =
+                (int)$salary['healthInsurance'] +
+                (int)$salary['welfarePension'] +
+                (int)$salary['residentTax'] +
+                (int)$salary['employmentInsurance'] +
+                (int)$salary['incomeTax'] +
+                (int)$salary['holding'];
 
-        $healthInsurance = [
-            'amount' => (-1) * (int)$salary['healthInsurance'],
-            'item' => '健康保険料',
-            'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($healthInsurance);
+            $deductionMove = [
+                'amount' => $deductionValue,
+                'item' => '予算移動',
+                'before_id' => self::INCOME_PURPOSE_ELEMENT_ID,
+                'after_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            MoveDao::insertMoveByArray('purpose', $deductionMove);
 
-        $welfarePension = [
-            'amount' => (-1) * (int)$salary['welfarePension'],
-            'item' => '厚生年金保険',
-            'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($welfarePension);
+            $healthInsurance = [
+                'amount' => (-1) * (int)$salary['healthInsurance'],
+                'item' => '健康保険料',
+                'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($healthInsurance);
 
-        $residentTax = [
-            'amount' => (-1) * (int)$salary['residentTax'],
-            'item' => '住民税',
-            'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($residentTax);
+            $welfarePension = [
+                'amount' => (-1) * (int)$salary['welfarePension'],
+                'item' => '厚生年金保険',
+                'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($welfarePension);
 
-        $employmentInsurance = [
-            'amount' => (-1) * (int)$salary['employmentInsurance'],
-            'item' => '雇用保険料',
-            'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($employmentInsurance);
+            $residentTax = [
+                'amount' => (-1) * (int)$salary['residentTax'],
+                'item' => '住民税',
+                'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($residentTax);
 
-        $incomeTax = [
-            'amount' => (-1) * (int)$salary['incomeTax'],
-            'item' => '所得税',
-            'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($incomeTax);
+            $employmentInsurance = [
+                'amount' => (-1) * (int)$salary['employmentInsurance'],
+                'item' => '雇用保険料',
+                'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($employmentInsurance);
 
-        $holding = [
-            'amount' => (-1) * (int)$salary['holding'],
-            'item' => '持株',
-            'kind_element_id' => self::SHARE_HELD_KIND_ELEMENT_ID,
-            'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
-            'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        BalanceDao::insertBalance($holding);
+            $incomeTax = [
+                'amount' => (-1) * (int)$salary['incomeTax'],
+                'item' => '所得税',
+                'kind_element_id' => self::DEDUCTION_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($incomeTax);
 
-        $takeSalary = 
-            (int)$salary['baseSalary'] +
-            (int)$salary['adjustmentSalary'] +
-            (int)$salary['transportation'] +
-            (int)$salary['holdingIncentives'] -
-            $deductionValue;
+            $holding = [
+                'amount' => (-1) * (int)$salary['holding'],
+                'item' => '持株',
+                'kind_element_id' => self::SHARE_HELD_KIND_ELEMENT_ID,
+                'purpose_element_id' => self::DEDUCTION_PURPOSE_ELEMENT_ID,
+                'place_element_id' => self::SKY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            BalanceDao::insertBalance($holding);
 
-        $mainMove = [
-            'amount' => $takeSalary,
-            'item' => '場所移動',
-            'before_id' => self::SKY_PLACE_ELEMENT_ID,
-            'after_id' => self::SALARY_PLACE_ELEMENT_ID,
-            'date' => (string)$salary['date']
-        ];
-        MoveDao::insertMoveByArray('place', $mainMove);
+            $takeSalary =
+                (int)$salary['baseSalary'] +
+                (int)$salary['adjustmentSalary'] +
+                (int)$salary['transportation'] +
+                (int)$salary['holdingIncentives'] -
+                $deductionValue;
 
+            $mainMove = [
+                'amount' => $takeSalary,
+                'item' => '場所移動',
+                'before_id' => self::SKY_PLACE_ELEMENT_ID,
+                'after_id' => self::SALARY_PLACE_ELEMENT_ID,
+                'date' => (string)$salary['date']
+            ];
+            MoveDao::insertMoveByArray('place', $mainMove);
+
+            \DB::commit();
+        } catch (Exception $e) {
+            \DB::rollback();
+        }
 
         return true;
     }
